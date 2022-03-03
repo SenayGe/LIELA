8ad6344b2ce8b55061be8224e2f2e9f65e48b482
Senay <senaykarisful@gmail.com>
2022-03-03 18:45:41 +0400

    modified onGenerateRoute

--- lib/Config/app_router.dart
+++ lib/Config/app_router.dart
@@ -4,15 +4,29 @@ import 'package:flutter/material.dart';
 import '../screens.dart';
 
 class AppRouter {
-  static Route onGenerateRoute (RouteSettings settings ){
+  static Route generateRoute (RouteSettings settings ){
+
+    // For passing arguments
+    final args = settings.arguments;
+
     print ('The route is ${settings.name}');
 
     switch (settings.name){
       case '/':
-        return HomeScreen.route();
+        return MaterialPageRoute(builder: (_) => HomeScreen());
       case HomeScreen.routeName:
         return HomeScreen.route();
-      case LoginScreen.route();
+      case '/LoginScreen':
+        if (args is String){
+          return MaterialPageRoute(
+              builder: (_) => LoginScreen(
+                data: args,
+              )
+          );
+        }
+        // If args is not of the correct type
+        return _errorRoute(); //TODO: Define _errorRoutee()
+      // case LoginScreen.route();
       default:
         return _errorRoute();
 
