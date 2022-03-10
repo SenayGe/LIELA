1721ec9b889ba25c2dfe1effaa1aba837ddd6359
Senay <senaykarisful@gmail.com>
2022-03-10 23:26:19 +0400

    added test users

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:liela/models/models.dart';
 
 class User extends Equatable{
   final int id;
@@ -25,6 +26,37 @@ class User extends Equatable{
     imageUrls,
     bio,
     jobTitle,
+  ];
+
+  static List<User> users = [
+    User(
+      id: 1,
+      name: 'Asm',
+      age: 20,
+      imageUrls: [
+        'https://unsplash.com/photos/yihlaRCCvd4',
+        'https://unsplash.com/photos/gsMSU6Pg_QI',
+        'https://unsplash.com/photos/SCIRnLEtqWc',
+        'https://unsplash.com/photos/Ri6DjpT7uPc',
+        'https://unsplash.com/photos/UJr_TCSObWY',
+      ],
+      bio: 'My bio goes here',
+      jobTitle: 'Job Title here'
+        ),
+    User(
+        id: 1,
+        name: 'Mer',
+        age: 25,
+        imageUrls: [
+          'https://unsplash.com/photos/Md73BFRHR0A',
+          'https://unsplash.com/photos/-lyP0OYsEtI',
+          'https://unsplash.com/photos/2lgTyxm79ls',
+          'https://unsplash.com/photos/NDUfajkNA6I',
+          'https://unsplash.com/photos/Olt577JtPM0',
+        ],
+        bio: 'My bio goes here',
+        jobTitle: 'Job Title here'
+    )
   ];
 
 }
