9ed9843a7d03abc5df6345962d045afeee00cc97
Senay <senaykarisful@gmail.com>
2022-03-11 23:09:39 +0400

    custom appbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:liela/models/models.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const String routeName = '/';
+  static const String routeName = '/ ';
   /*static Route route(){
     return MaterialPageRoute(
         builder: (_) => HomeScreen(),
@@ -12,11 +13,54 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,//Text('Liela', style: TextStyle(color: Colors.red.shade300),),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              fit: BoxFit.contain,
+              height: 38,
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('LIELA', style: TextStyle(color: Colors.red.shade300),)),
+          ],
+        ),
+        actions: [
+          IconButton(icon: Icon(Icons.filter_alt_outlined, color: Theme.of(context).primaryColor,) , onPressed: (){})
+        ],
+
+      ),
+      //body: UserCard (user: User.users[0]),
+    );
+  }
+
+  Image appLogo = new Image(
+      image: new ExactAssetImage("assets/images/logo.png"),
+      height: 28.0,
+      width: 20.0,
+      alignment: FractionalOffset.center);
+}
+
+/*class UserCard extends StatelessWidget {
+  final User user;
+  const UserCard({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 1.4,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        Container(
+
+        ),
       ),
     );
   }
-}
+}*/
+
 
 
