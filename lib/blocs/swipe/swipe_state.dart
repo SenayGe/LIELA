fbe69f15e381444df00e82eb45954fbb1861afd1
Senay <senaykarisful@gmail.com>
2022-05-04 22:55:08 +0400

    swipe state

--- lib/blocs/swipe/swipe_state.dart
+++ lib/blocs/swipe/swipe_state.dart
@@ -1,11 +1,18 @@
+part of 'swipe_bloc.dart';
+
 
 
 import 'package:equatable/equatable.dart';
 
 abstract class SwipeState extends Equatable{
+  const SwipeState();
+
+  @override
+  List<Object?> get props => [];
+
 
 }
 
 class SwipeLoading extends SwipeState{
-  
+
 }
