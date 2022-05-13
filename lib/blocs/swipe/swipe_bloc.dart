27780906adeb4602c30ee33b1dc97d1ab1a7c157
Senay <senaykarisful@gmail.com>
2022-05-13 23:59:28 +0400

    defined event mapped state actions

--- lib/blocs/swipe/swipe_bloc.dart
+++ lib/blocs/swipe/swipe_bloc.dart
@@ -10,7 +10,28 @@ class SwipeBloc extends Bloc<SwipeEvent, SwipeState>{
   SwipeBloc(): super(SwipeLoading());
 
   @override
-  Stream<SwipeState> mapEventsTosState{
+  Stream<SwipeState> mapEventsTosState (SwipeEvent event) async* {
+    if (event is LoadUsersEvent){
+      yield* _mapLoadUsersToState(event);
+    }
+    if (event is SwipeLeftEvent){
+      yield* _mapSwipeLeftToState(event, state);
+    }
+    if (event is SwipeRightEvent){
+      yield* _mapSwipeRightToState(event, state);
+    }
+  }
 
-}
+  Stream<SwipeState> _mapLoadUsersToState (LoadUsersEvent event) async* {
+      yield SwipeLoaded(users: event.users);
+  }
+  Stream<SwipeState> _mapSwipeLeftToState (SwipeLeftEvent event, SwipeState state) async* {
+    if (state is SwipeLoaded){
+      try{
+        yield SwipeLoaded(users: List.from(state.users)..remove(event.user));
+      }
+      catch(_){}
+    }
+
+  }
 }
