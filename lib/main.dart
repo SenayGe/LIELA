683d19b4300ebd7f2e611f153cd8581b18cd995b
Senay <senaykarisful@gmail.com>
2022-05-23 23:53:46 +0400

    bloc provider implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:liela/blocs/swipe/swipe_bloc.dart';
 import 'package:liela/screens.dart';
 import 'package:liela/config/app_router.dart';
+
+import 'models/models.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,25 +14,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Liela dating app',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.grey ,
-      ),
-      initialRoute: '/',
-      onGenerateRoute: AppRouter.generateRoute,
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (_) =>
+                  SwipeBloc()..add(LoadUsersEvent(users: User.users)))
+        ],
+        child: MaterialApp(
+          title: 'Liela dating app',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.grey,
+          ),
+          initialRoute: '/',
+          onGenerateRoute: AppRouter.generateRoute,
+        ));
   }
 }
-
-
-
