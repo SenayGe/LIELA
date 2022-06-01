8f183712f71fee49fd70483e105b682f4dbdd3f1
Senay <senaykarisful@gmail.com>
2022-06-01 22:20:43 +0400

    done with buttons

--- lib/screens/user/user_screen.dart
+++ lib/screens/user/user_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:liela/models/models.dart';
+import 'package:liela/widgets/reaction_button.dart';
 
 class UsersScreen extends StatelessWidget {
   static const String routeName = '/users';
@@ -20,12 +22,51 @@ class UsersScreen extends StatelessWidget {
       extendBodyBehindAppBar: true,
       body: SizedBox(
         height: MediaQuery.of(context).size.height / 2,
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(User.users[0].imageUrls[1]),
-                  fit: BoxFit.cover),
-              borderRadius: BorderRadius.circular(15.0)),
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(User.users[0].imageUrls[1]),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(15.0)),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ReactionButton(
+                      width: 60,
+                      height: 60,
+                      color: Colors.white,
+                      icon: Icons.clear_rounded,
+                      iconColor: Colors.redAccent,
+                    ),
+                    ReactionButton(
+                      width: 80,
+                      height: 80,
+                      color: Colors.white,
+                      icon: Icons.favorite_rounded,
+                      iconColor: Colors.greenAccent.shade700),
+                    ReactionButton(
+                      width: 60,
+                      height: 60,
+                      color: Colors.white,
+                      icon: Icons.watch_later_rounded,
+                      iconSize: 38,
+                      iconColor: Colors.deepPurpleAccent,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
