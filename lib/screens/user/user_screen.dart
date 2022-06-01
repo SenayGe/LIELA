9d7466d25a46f56892be0b277cab857c630167e0
Senay <senaykarisful@gmail.com>
2022-06-01 22:02:48 +0400

    finished profile pic

--- lib/screens/user/user_screen.dart
+++ lib/screens/user/user_screen.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:liela/models/models.dart';
 
-class UserScreen extends StatelessWidget {
+class UsersScreen extends StatelessWidget {
+  static const String routeName = '/users';
 
+  static Route route() {
+    return MaterialPageRoute(
+        settings: RouteSettings(name: routeName),
+        builder: (context) => UsersScreen());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Users'),
-        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height / 2,
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(User.users[0].imageUrls[1]),
+                  fit: BoxFit.cover),
+              borderRadius: BorderRadius.circular(15.0)),
+        ),
       ),
     );
   }
