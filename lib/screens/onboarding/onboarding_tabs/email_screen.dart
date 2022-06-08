be19aa7e5246c96442531010b1e43160ca4dbdfc
Senay <senaykarisful@gmail.com>
2022-06-09 01:27:58 +0400

    initial email screen done

--- lib/screens/onboarding/onboarding_tabs/email_screen.dart
+++ lib/screens/onboarding/onboarding_tabs/email_screen.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:liela/Screens/onboarding/widgets/custom_button.dart';
+import 'package:liela/Screens/onboarding/widgets/custom_header.dart';
+import 'package:liela/Screens/onboarding/widgets/custom_text_field.dart';
 
-class EmailTab extends StatelessWidget {
+class Email extends StatelessWidget {
   final TabController tabController;
-  const EmailTab({ required this.tabController, Key? key}) : super(key: key);
+  const Email({ required this.tabController, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
+      child: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              CustomHeader(tabController: tabController, text: 'Add your email',),
+              SizedBox(height: 50.0,),
+              CustomTextField(tabController: tabController, text: 'johndoe@gmail.com'),
+            ],
+          ),
+          SizedBox(height: 50.0),
+          CustomButton(tabController: tabController, text: 'CONTINUE',)
+        ],
+      ),
+    );
   }
 }
