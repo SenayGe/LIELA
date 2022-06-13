31c6164c2ceea64c83103707f663274db79d91c0
Senay <senaykarisful@gmail.com>
2022-06-14 02:03:03 +0400

    Email verification screen done

--- lib/screens/onboarding/onboarding_tabs/email_verfication_screen.dart
+++ lib/screens/onboarding/onboarding_tabs/email_verfication_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:liela/Screens/onboarding/widgets/custom_button.dart';
+import 'package:liela/Screens/onboarding/widgets/custom_header.dart';
+import 'package:liela/Screens/onboarding/widgets/custom_text_field.dart';
 
 class EmailVerification extends StatelessWidget {
   final TabController tabController;
@@ -6,6 +9,22 @@ class EmailVerification extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
+      child: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              CustomHeader(tabController: tabController, text: 'Enter email verification code',),
+              SizedBox(height: 50.0,),
+              CustomTextField(tabController: tabController, text: 'Verification code'),
+            ],
+          ),
+          SizedBox(height: 50.0),
+          CustomButton(tabController: tabController, text: 'CONTINUE',)
+        ],
+      ),
+    );
   }
 }
