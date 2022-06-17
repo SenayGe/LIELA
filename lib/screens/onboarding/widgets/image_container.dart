6ea8e78c5dccc423ac34124bcbe5af1b8d016055
Senay <senaykarisful@gmail.com>
2022-06-17 16:37:48 +0400

    created custom image containers for uploading photos

---
+++ lib/screens/onboarding/widgets/image_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomImageContainer extends StatelessWidget {
+  final TabController tabController;
+  const CustomImageContainer({required this.tabController, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0, right: 10.0 ),
+      child: Container(
+        height: 130,
+        width: 100,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.symmetric(
+                vertical: BorderSide(color: Theme.of(context).primaryColor, width: 1.5),
+                horizontal: BorderSide(color: Theme.of(context).primaryColor, width: 1.5)
+            )
+        ),
+      ),
+    );
+  }
+}
