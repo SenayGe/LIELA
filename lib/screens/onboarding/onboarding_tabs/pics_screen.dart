1a8da7baf8e9d74a352b5cb92581a9098ad4c151
Senay <senaykarisful@gmail.com>
2022-06-17 16:55:09 +0400

    Photos uploading screen done

--- lib/screens/onboarding/onboarding_tabs/pics_screen.dart
+++ lib/screens/onboarding/onboarding_tabs/pics_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:liela/Screens/onboarding/widgets/custom_button.dart';
+import 'package:liela/Screens/onboarding/widgets/custom_header.dart';
+import 'package:liela/Screens/onboarding/widgets/image_container.dart';
 
 class Pics extends StatelessWidget {
   final TabController tabController;
@@ -6,6 +9,25 @@ class Pics extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 50.0),
+      child: Column(
+        children: [
+          CustomHeader(tabController: tabController, text: 'Upload your photos'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomImageContainer(tabController: tabController),
+              CustomImageContainer(tabController: tabController),
+              CustomImageContainer(tabController: tabController),
+            ],
+          ),
+
+          SizedBox(height: 50.0),
+          CustomButton(tabController: tabController, text: 'CONTINUE',)
+        ],
+      ),
+
+    );
   }
 }
--- lib/screens/onboarding/widgets/image_container.dart
+++ lib/screens/onboarding/widgets/image_container.dart
@@ -8,20 +8,23 @@ class CustomImageContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0, right: 10.0 ),
+      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0, right: 10.0),
       child: Container(
         height: 130,
         width: 100,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             border: Border.symmetric(
-                vertical: BorderSide(color: Theme.of(context).primaryColor, width: 1.5),
-                horizontal: BorderSide(color: Theme.of(context).primaryColor, width: 1.5)
-            )
-        ),
+                vertical: BorderSide(
+                    color: Theme.of(context).primaryColor, width: 1.5),
+                horizontal: BorderSide(
+                    color: Theme.of(context).primaryColor, width: 1.5))),
         child: Align(
             //alignment: Alignment.bottomRight,
-            child: Icon (Icons.add_circle, color: Colors.pink.shade300 ,)),
+            child: IconButton(
+          icon: Icon(Icons.add_circle, color: Colors.pink.shade300),
+          onPressed: () {},
+        )),
       ),
     );
   }
