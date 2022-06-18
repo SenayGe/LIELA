34f089310d927d9973642d5fb27245f84fb27063
Senay <senaykarisful@gmail.com>
2022-06-18 15:43:48 +0400

    bio screen done

--- lib/screens/onboarding/onboarding_tabs/bio_screen.dart
+++ lib/screens/onboarding/onboarding_tabs/bio_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:liela/Screens/onboarding/widgets/tag_container.dart';
+import 'package:liela/Screens/onboarding/widgets/widgets.dart';
 
 class Bio extends StatelessWidget {
   final TabController tabController;
@@ -6,6 +8,45 @@ class Bio extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 50.0),
+      child: Column(
+        children: [
+          Column(
+
+            children: [
+              CustomHeader(tabController: tabController, text: 'Describe yourself in a few words'),
+              SizedBox(height: 30.0,),
+              CustomTextField(tabController: tabController, text: 'Your BIO'),
+              SizedBox(height: 30.0,),
+              CustomHeader(tabController: tabController, text: 'What are your interests'),
+              SizedBox(height: 30.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomTagContainer(tabcontroller: tabController, text: 'Sports'),
+                  CustomTagContainer(tabcontroller: tabController, text: 'Mezmurrr'),
+                  CustomTagContainer(tabcontroller: tabController, text: 'Football'),
+                  CustomTagContainer(tabcontroller: tabController, text: 'Sports'),
+                ],
+              ),
+              Row(
+                children: [
+                  CustomTagContainer(tabcontroller: tabController, text: 'Football'),
+                  CustomTagContainer(tabcontroller: tabController, text: 'Sports'),
+                  CustomTagContainer(tabcontroller: tabController, text: 'Sports'),
+                  CustomTagContainer(tabcontroller: tabController, text: 'Mezmurrr'),
+
+                ],
+              )
+            ],
+          ),
+
+          SizedBox(height: 50.0),
+          CustomButton(tabController: tabController, text: 'CONTINUE',)
+        ],
+      ),
+
+    );;
   }
 }
