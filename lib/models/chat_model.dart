a9bac83585b4ebd6eeac844ab122b6903bc41816
Senay <senaykarisful@gmail.com>
2022-06-21 00:03:36 +0400

    minor fix

--- lib/models/chat_model.dart
+++ lib/models/chat_model.dart
@@ -4,24 +4,24 @@ import 'package:liela/models/message_model.dart';
 class Chat extends Equatable {
   final int id;
   final int userId;
-  final int otherUserId;
+  final int matchedUserId;
   final List<Message> messages;
 
   Chat(
       {required this.id,
       required this.userId,
-      required this.otherUserId,
+      required this.matchedUserId,
       required this.messages});
 
   @override
   // TODO: implement props
-  List<Object?> get props => [id, userId, otherUserId, messages];
+  List<Object?> get props => [id, userId, matchedUserId, messages];
 
   static List<Chat> sampleChats = [
     Chat(
         id: 1,
         userId: 1,
-        otherUserId: 2,
+        matchedUserId: 2,
         messages: Message.sampleMessages
             .where((msg) => ((msg.senderId == 1 && msg.receiverId == 2) ||
                 (msg.senderId == 2 && msg.receiverId == 1)))
@@ -29,7 +29,7 @@ class Chat extends Equatable {
     Chat(
         id: 2,
         userId: 1,
-        otherUserId: 3,
+        matchedUserId: 3,
         messages: Message.sampleMessages
             .where((msg) => ((msg.senderId == 1 && msg.receiverId == 3) ||
             (msg.senderId == 3 && msg.receiverId == 1)))
@@ -37,7 +37,7 @@ class Chat extends Equatable {
     Chat(
         id: 3,
         userId: 1,
-        otherUserId: 4,
+        matchedUserId: 4,
         messages: Message.sampleMessages
             .where((msg) => ((msg.senderId == 1 && msg.receiverId == 4) ||
             (msg.senderId == 4 && msg.receiverId == 1)))
@@ -45,7 +45,7 @@ class Chat extends Equatable {
     Chat(
         id: 4,
         userId: 1,
-        otherUserId: 5,
+        matchedUserId: 5,
         messages: Message.sampleMessages
             .where((msg) => ((msg.senderId == 1 && msg.receiverId == 4) ||
             (msg.senderId == 4 && msg.receiverId == 1)))
