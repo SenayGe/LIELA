6f76c425c61f8a26f56b2fc720438b0d9ea26031
Senay <senaykarisful@gmail.com>
2022-07-02 22:33:37 +0400

    chat screen done

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -32,57 +32,82 @@ class ChatScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: userMatch.chats[0].messages.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                  title: userMatch.chats[0].messages[index].senderId == 1
-                      ? Align(
-                          alignment: Alignment.topRight,
-                          child: Container(
-                              padding: EdgeInsets.all(8),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                color: Theme.of(context).backgroundColor,
-                              ),
-                              child: Text(
-                                  userMatch.chats[0].messages[index].message,
-                                  style:
-                                      Theme.of(context).textTheme.bodyText2)),
-                        )
-                      : Align(
-                          alignment: Alignment.topLeft,
-                          child: Row(
-                            children: [
-                              CircleAvatar(
-                                radius: 17,
-                                backgroundImage: NetworkImage(
-                                    userMatch.matchedUser.imageUrls[1]),
-                              ),
-                              SizedBox(width: 8,),
-                              Container(
-                                padding: EdgeInsets.all(8),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  color: Colors.indigo.shade400,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Container(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: userMatch.chats[0].messages.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                        title: userMatch.chats[0].messages[index].senderId == 1
+                            ? Align(
+                                alignment: Alignment.topRight,
+                                child: Container(
+                                    padding: EdgeInsets.all(8),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color: Theme.of(context).backgroundColor,
+                                    ),
+                                    child: Text(
+                                        userMatch
+                                            .chats[0].messages[index].message,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2)),
+                              )
+                            : Align(
+                                alignment: Alignment.topLeft,
+                                child: Row(
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 17,
+                                      backgroundImage: NetworkImage(
+                                          userMatch.matchedUser.imageUrls[1]),
+                                    ),
+                                    SizedBox(
+                                      width: 8,
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(8),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        color: Colors.indigo.shade400,
+                                      ),
+                                      child: Text(
+                                        userMatch
+                                            .chats[0].messages[index].message,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2!
+                                            .copyWith(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                child: Text(
-                                  userMatch.chats[0].messages[index].message,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText2!
-                                      .copyWith(color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ));
-            },
+                              ));
+                  },
+                ),
+              ),
+            ),
           ),
-        ),
+          Container(
+            padding: EdgeInsets.all(20),
+            height: 100,
+            child: Row(
+              children: [
+                IconButton (icon: Icon(Icons.send_outlined),onPressed: (){},),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(filled: true, fillColor: Colors.white , hintText: 'Type here', ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
