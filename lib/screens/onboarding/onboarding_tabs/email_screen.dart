10078ae72f1a6d5f7f36aa285597fa7af9ad752c
Senay <senaykarisful@gmail.com>
2022-07-05 23:40:47 +0400

    user registration with firebase done

--- lib/screens/onboarding/onboarding_tabs/email_screen.dart
+++ lib/screens/onboarding/onboarding_tabs/email_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:liela/Screens/onboarding/widgets/custom_button.dart';
 import 'package:liela/Screens/onboarding/widgets/custom_header.dart';
 import 'package:liela/Screens/onboarding/widgets/custom_text_field.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class Email extends StatelessWidget {
   final TabController tabController;
@@ -9,6 +10,8 @@ class Email extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
       child: Column(
@@ -18,11 +21,25 @@ class Email extends StatelessWidget {
             children: [
               CustomHeader(tabController: tabController, text: 'Add your email',),
               SizedBox(height: 50.0,),
-              CustomTextField(tabController: tabController, text: 'johndoe@gmail.com'),
+              CustomTextField(tabController: tabController, text: 'johndoe@gmail.com', textEditingController: emailController,),
+              SizedBox(height: 50.0,),
+              CustomHeader(tabController: tabController, text: 'Choose your password',),
+              SizedBox(height: 50.0,),
+              CustomTextField(tabController: tabController, text: 'password', textEditingController: passwordController,),
+              SizedBox(height: 50.0,),
+
             ],
+
           ),
           SizedBox(height: 50.0),
-          CustomButton(tabController: tabController, text: 'CONTINUE',)
+          StepProgressIndicator(
+            totalSteps: 6,
+            currentStep: tabController.index + 1,
+            selectedColor: Theme.of(context).primaryColor,
+            unselectedColor: Theme.of(context).backgroundColor,
+          ),
+          SizedBox(height: 30.0),
+          CustomButton(tabController: tabController, text: 'NEXT', emailController: emailController, passwordController: passwordController,)
         ],
       ),
     );
