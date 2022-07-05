c91e7a8e06baf11e05c4bd2ea75fb0a3d60646e0
Senay <senaykarisful@gmail.com>
2022-07-05 23:29:33 +0400

    added text-editing controller

--- lib/screens/onboarding/widgets/custom_text_field.dart
+++ lib/screens/onboarding/widgets/custom_text_field.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final TabController tabController;
+  final TextEditingController textEditingController;
   final String text;
-  const CustomTextField({Key? key, required this.tabController, required this.text}) : super(key: key);
+  const CustomTextField({Key? key, required this.tabController, required this.text, required this.textEditingController}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       keyboardType: TextInputType.emailAddress,
+      controller: textEditingController,
       decoration: InputDecoration(
         filled: true,
         fillColor: Colors.white,
