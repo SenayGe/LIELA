8de9f4f56bb36bb9b7b492bfd3e0c8e7d3f4c88f
Senay <senaykarisful@gmail.com>
2022-07-07 23:55:35 +0400

    implemented sign-up function

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,22 @@
+
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:liela/repositories/base_auth_repository.dart';
+
+class AuthRepository extends BaseAuthRepository {
+  final auth.FirebaseAuth _firebaseAuth;
+
+  AuthRepository ({auth.FirebaseAuth? firebaseAuth}) :
+      _firebaseAuth = firebaseAuth ?? auth.FirebaseAuth.instance;
+  @override
+  Future<auth.User?> signUp({required String email, required String password}) async{
+    try{
+      final credential = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      return (credential.user);
+    } catch(_){}
+  }
+
+  @override
+  // TODO: implement user
+  Stream<auth.User?> get user => _firebaseAuth.userChanges();
+  
+}
