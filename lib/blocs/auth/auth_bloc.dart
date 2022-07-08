4f52e983757c9db8b2d63e34eb04f84b63560301
Senay <senaykarisful@gmail.com>
2022-07-08 22:44:06 +0400

    created auth bloc

---
+++ lib/blocs/auth/auth_bloc.dart
@@ -0,0 +1,5 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+
+part 'auth_event.dart';
+part 'auth_state.dart';
---
+++ lib/blocs/auth/auth_state.dart
@@ -0,0 +1,26 @@
+part of 'auth_bloc.dart';
+
+enum AuthStatus { UNKNOWN, AUTHENTICATED, UNAUTHENTICATED }
+
+abstract class AuthState extends Equatable {
+  final AuthStatus status;
+  final auth.User? user;
+
+  const AuthState._({this.status = AuthStatus.UNKNOWN, this.user});
+
+  const AuthState.unknown() : this._();
+
+  const AuthState.authenticated({required auth.User user})
+      : this._(
+          status: AuthStatus.AUTHENTICATED,
+          user: user,
+        );
+
+  const AuthState.unauthenticated()
+      : this._(status: AuthStatus.UNAUTHENTICATED);
+
+  @override
+  List<Object?> get props => [status, user];
+}
+
+//class AuthInitial extends AuthState {}
