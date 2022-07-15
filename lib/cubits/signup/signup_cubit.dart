e5c1f5efdae046bc8b8f5ce49d06a0129f963c77
Senay <senaykarisful@gmail.com>
2022-07-15 16:48:58 +0400

    signup cubit implemented

--- lib/cubits/signup/signup_cubit.dart
+++ lib/cubits/signup/signup_cubit.dart
@@ -1,4 +1,31 @@
-
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:liela/repositories/auth_repository.dart';
 
 part 'signup_state.dart';
+
+class SignupCubit extends Cubit<SignupState> {
+  final AuthRepository _authRepository;
+
+  SignupCubit({required authRepository})
+      : this._authRepository = authRepository,
+        super(SignupState.initial());
+
+  void emailChanged(newEmail) {
+    emit(state.copyWith(email: newEmail));
+  }
+
+  void passwordChanged(newPassword) {
+    emit(state.copyWith(password: newPassword));
+  }
+
+  void signupWithCredential() async {
+    if (state.isValid()) {
+      try {
+        await _authRepository.signUp(
+            email: state.email, password: state.password);
+        emit(state.copyWith(status: SignupStatus.SUCCESS));
+      } catch (_) {}
+    }
+  }
+}
--- lib/cubits/signup/signup_state.dart
+++ lib/cubits/signup/signup_state.dart
@@ -1,24 +1,28 @@
-
 part of 'signup_cubit.dart';
 
-enum SignupStatus {INITIAL, SUBMITTING, SUCCESS, ERROR};
+enum SignupStatus { INITIAL, SUBMITTING, SUCCESS, ERROR }
 
 class SignupState extends Equatable {
   final String email;
   final String password;
   final SignupStatus status;
 
-  const SignupState ({required this.email, required this.password, required this.status});
+  const SignupState(
+      {required this.email, required this.password, required this.status});
 
-  factory SignupState.initial () {
+  factory SignupState.initial() {
     return SignupState(email: '', password: '', status: SignupStatus.INITIAL);
   }
 
-  SignupState copyWith ({String? email, String? password, SignupStatus? status}){
-    return SignupState(email: email ?? this.email, password: password ?? this.password, status: status ?? this.status);
+  SignupState copyWith({String? email, String? password, SignupStatus? status}) {
+    return SignupState(
+        email: email ?? this.email,
+        password: password ?? this.password,
+        status: status ?? this.status);
   }
-  @override
-  // TODO: implement props
-  List<Object?> get props => [email, password, status];
 
+  bool isValid() => email.isNotEmpty && password.isNotEmpty;
+
+  @override
+  List<Object?> get props => [email, password, status];
 }
