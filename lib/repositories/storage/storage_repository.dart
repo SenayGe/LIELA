14941a70d0e388c9ab0a6a4ffe13944379a814c5
Senay <senaykarisful@gmail.com>
2022-10-05 14:03:44 +0400

    firebase storage repository

---
+++ lib/repositories/storage/storage_repository.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:cross_file/src/types/interface.dart';
+import 'package:liela/repositories/storage/base_storage_repository.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+
+class StorageRepository extends BaseStorageRepository {
+
+  final firebase_storage.FirebaseStorage firebaseStorage = firebase_storage.FirebaseStorage.instance;
+  @override
+  Future<void> uploadImage(XFile image) async{
+
+    try{
+      firebaseStorage.ref('user_1/${image.path}').putFile(File(image.path));
+    }
+    catch(_){}
+
+  }
+
+}
