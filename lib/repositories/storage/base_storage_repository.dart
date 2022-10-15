0f595069b171c6a3519065b4059ee8a09ecd6e4d
Senay <senaykarisful@gmail.com>
2022-10-15 23:45:47 +0400

    minor fix

--- lib/repositories/storage/base_storage_repository.dart
+++ lib/repositories/storage/base_storage_repository.dart
@@ -4,4 +4,5 @@ import 'package:image_picker/image_picker.dart';
 abstract class BaseStorageRepository{
 
   Future<void> uploadImage(XFile image);
+  Future<String> getDownloadUrl(String imageName);
 }
