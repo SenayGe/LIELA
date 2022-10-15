50bddc6b687c90b2e745a0d2034ec89e2586da4e
Senay <senaykarisful@gmail.com>
2022-10-15 23:58:07 +0400

    minor fixes

--- lib/blocs/images/images_event.dart
+++ lib/blocs/images/images_event.dart
@@ -2,4 +2,20 @@ part of 'images_bloc.dart';
 
 abstract class ImagesEvent extends Equatable {
   const ImagesEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoadImages extends ImagesEvent {}
+
+class UpdateImages extends ImagesEvent {
+
+  final List<dynamic> imageUrls;
+
+  UpdateImages ({this.imageUrls = const []})
+
+  @override
+  List<Object> get props => [imageUrls];
+
 }
